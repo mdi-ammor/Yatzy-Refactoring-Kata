7c3c07d03844661810f3947ebd49cfea86478498
mdi-ammor <mdi.ammor@gmail.com>
2019-10-29 15:26:49 +0100

    Update Yatzy.java

--- Yatzy.java
+++ Yatzy.java
@@ -16,7 +16,7 @@ public class Yatzy {
 
     public static int yatzy(int... dice) {
     	
-		final int[] counts = new int[6];
+	final int[] counts = new int[6];
 		
     	IntStream.of(dice).forEach(e -> counts[e-1]++);
     	
@@ -95,131 +95,101 @@ public class Yatzy {
 										.anyMatch(n -> n >= 2) ? i.get() : -1;
 
 		return (6 - index) * 2;
-		//return 0;
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
+    public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
+        List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
+        
+        AtomicInteger n = new AtomicInteger(0);
+        AtomicInteger score = new AtomicInteger(0);
+        IntStream.range(0, 6)
+        	.filter(i -> valuesList.get(6-i-1) >= 2)
+        	.forEach(item -> {
+        		n.incrementAndGet();
+        		score.addAndGet(item*2);
+        	});
+        
+        if (n.get() == 2)
+            return score.get() * 2;
         else
             return 0;
+        
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+    	List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
+    	AtomicInteger res = new AtomicInteger(0);
+    	IntStream.range(0, 6)
+    		.filter(i -> valuesList.get(i) >= 4)
+    		.forEach(item -> {
+    			res.addAndGet((item*4)+4);
+    		});
+
+    	if(res.get()!=0) {
+    		return res.get();
+    	}
+    	return 0;
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
+    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+    	List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
         for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
+            if (valuesList.get(i) >= 3)
                 return (i+1) * 3;
         return 0;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
+    	List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
+    	if (IntStream.range(0, valuesList.size()).filter(i -> valuesList.get(i) == i).findFirst().isPresent()) {
+    		return 15;
+		}
         return 0;
     }
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)  {
+    	List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
+    	if (IntStream.range(0, valuesList.size()).filter(i -> valuesList.get(i) == i).findFirst().isPresent()) {
+    		return 20;
+		}
         return 0;
     }
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
+    public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
+    	AtomicBoolean value2 = new AtomicBoolean(false);
+    	AtomicInteger at2 = new AtomicInteger(0);
+    	AtomicBoolean value3 = new AtomicBoolean(false);
+    	AtomicInteger at3 = new AtomicInteger(0);
 
+        List<Integer> valuesList = buildValuesList(d1, d2, d3, d4, d5);
 
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
+        IntStream.range(0, 6)
+        	.filter(i -> valuesList.get(i) == 2)
+        	.forEach(item -> {
+        		value2.set(true);
+        		at2.addAndGet(item+1);
+        	});
+        IntStream.range(0, 6)
+        	.filter(i -> valuesList.get(i) == 3)
+        	.forEach(item -> {
+        		value3.set(true);
+        		at3.addAndGet(item+1);
+        	});
+        
+        if (value2.get() && value3.get())
+            return at2.get() * 2 + at3.get() * 3;
         else
             return 0;
     }
+   
+    private static List<Integer> buildValuesList(int d1, int d2, int d3, int d4, int d5) {
+		int[] list = new int[6];
+         List<Integer> parametersList = new ArrayList<>(Arrays.asList(d1, d2, d3, d4, d5));
+         List<Integer> valuesList = Arrays.stream(list).boxed().collect(Collectors.toList());
+         IntStream.range(0, valuesList.size()-1)
+         	.forEach(i -> valuesList.set(parametersList.get(i) - 1, valuesList.get(parametersList.get(i) - 1) + 1)
+         	);
+		return valuesList;
+	}
 }
 
